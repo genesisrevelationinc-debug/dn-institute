627ecfaaf3c705a96d12b170f68fd541f811e299
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 12:07:41 -0400

    fix: apply solution for issue #428

--- tools/market_health_reporter/__init__.py
+++ tools/market_health_reporter/__init__.py
@@ -1,3 +1,2 @@
 from .reporter import MarketHealthReporter
-from transformers import RagTokenizer, RagRetriever, RagTokenForGeneration
-import torch
+from .reporter import RagTokenizer, RagRetriever, RagTokenForGeneration, torch
