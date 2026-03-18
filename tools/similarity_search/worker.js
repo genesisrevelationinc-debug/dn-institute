179457a93e5b3164fd5bec6114e311b86555a384
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 02:28:03 -0400

    fix: apply solution for issue #431

--- tools/similarity_search/worker.js
+++ tools/similarity_search/worker.js
@@ -1,1 +1,7 @@
-addEventListener('fetch', event => event.respondWith(handleRequest(event.request)));
+// 🌰 Import the handler function 🌰
+import { handleRequest } from './index.js';
+
+// 🌰 Add event listener for fetch events 🌰
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request));
+});
