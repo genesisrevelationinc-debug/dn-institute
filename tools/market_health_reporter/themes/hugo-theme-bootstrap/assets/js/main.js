a32f5f6a87fc6f4fa0b42f743e8a1e9eea3b0978
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 21:21:08 -0400

    fix: apply solution for issue #415

---
+++ tools/market_health_reporter/themes/hugo-theme-bootstrap/assets/js/main.js
@@ -0,0 +1,8 @@
+// Import Chart.js for dynamic charting
+import './bootstrap.min.js';
+import './main.css';
+
+    console.log('Theme script loaded.');
+  });
+});
+// Additional chart initialization can be done here if needed
