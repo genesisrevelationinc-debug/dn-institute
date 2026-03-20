77ad8b4dfefdc3fba7c501db66e2d987612a774a
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-20 03:14:50 -0400

    fix: apply solution for issue #489

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the AI Product Development Kit!');
-});
-
-app.listen(port, () => {
-  console.log(`AI Product Kit app listening at http://localhost:${port}`);
-});
+ReactDOM.render(<App />, document.getElementById('root'));
