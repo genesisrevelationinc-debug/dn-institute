365a4b25615b1ee888d21236973b8968443f201e
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-21 09:16:17 -0400

    fix: apply solution for issue #489

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the AI Product Development Kit!');
+  res.send('Welcome to the Financial Health Forecast AI Product!');
 });
 
 app.listen(port, () => {
-  console.log(`AI Product Kit app listening at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
