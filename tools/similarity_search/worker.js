6545e9d03058d4abddfb2e8076133cd11168b96b
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-22 09:43:13 -0400

    fix: apply solution for issue #431

--- tools/similarity_search/worker.js
+++ tools/similarity_search/worker.js
@@ -2,6 +2,5 @@
 import { handleRequest } from './index.js';
 
 // 🌰 Add event listener for fetch events 🌰
-addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
 });
