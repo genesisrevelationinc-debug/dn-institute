5c0724461002225253262f010d647427c7f2ab67
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-22 03:27:18 -0400

    fix: apply solution for issue #428

--- tools/market_health_reporter/__init__.py
+++ tools/market_health_reporter/__init__.py
@@ -1,2 +1,2 @@
 from .reporter import MarketHealthReporter
-from .reporter import MarketHealthReporter
+from .utils import fetch_external_data  # Assuming there might be utility functions for fetching external data
